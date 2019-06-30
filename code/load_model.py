3d7de4e2570181f729c53c2c964a404e67df14ff
Your Name <you@example.com>
2019-06-30 17:22:13 +0530

    Trained on more data, stored in model3.h5

--- code/load_model.py
+++ code/load_model.py
@@ -1,40 +1,46 @@
 # %%
-import keras 
+import keras
 from keras.models import load_model
 import cv2
 import os
-import numpy as np 
-# %% 
-model = load_model('model2.h5')
+import numpy as np
+# %%
+model = load_model('upsampling saved models/model3.h5')
 
-#%%
+# %%
 '''Load datasets'''
 
 test_set = []
 
-path_test = ('./Linnaeus 5 32x32/test/')
-path2_test = ('./Linnaeus 5 64x64/test/')
+path_test = ('./data/Linnaeus 5 32x32/test/')
+path2_test = ('./data/Linnaeus 5 64x64/test/')
 
-for r1,d1,f1 in os.walk(path_test):
+for r1, d1, f1 in os.walk(path_test):
     f1 = f1[:1]
     for files in f1:
         test_set.append(cv2.imread(r1 + '/' + files))
+        print(r1+'/' + files)
 
-print(len(test_set))
 test_set = np.array(test_set)
 print(test_set.shape)
-#%%
-result = model.predict(test_set,batch_size = 100, verbose=1)
+# %%
+result = model.predict(test_set, batch_size=100, verbose=1)
 print(result.shape)
 
-#%%
+# %%
 result_list = list(result)
-len(result_list)
-
-#%%
+test_set = list(test_set)
+# %%
 i = 0
 for r in result_list:
-    cv2.imwrite(str(i)+'.jpg',r)
+    cv2.imwrite(str(i)+'.jpg', r)
     i += 1
-#%%
+# %%
+for i in range(5):
+    cv2.imshow('original', test_set[i])
+    cv2.imshow('pred', result_list[i])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 #%%
--- code/upsampling_model.py
+++ code/upsampling_model.py
@@ -1,86 +1,94 @@
 # %%
-import os
-import cv2
-import tensorflow as tf
-import keras 
-import keras.layers as L
+print('Importing libraries ....')
+
+import keras
+from keras.callbacks import TensorBoard
+from keras.layers import Conv2D, BatchNormalization, MaxPool2D, UpSampling2D
+from keras.models import Sequential
 import numpy as np
-from sklearn.model_selection import train_test_split
-
+import cv2
+import os
+from tqdm import tqdm
 # %%
+
+
 def make_model():
-    model = keras.models.Sequential()
-    model.add(L.Conv2D(filters=32, kernel_size=(3, 3),activation='relu',
-                       input_shape=(480, 853, 3), padding='same'))
-    model.add(L.BatchNormalization())
-    model.add(L.MaxPool2D(pool_size=(2,2),padding='same'))
-    model.add(L.Conv2D(filters=16, kernel_size=(3, 3), padding='same',activation='relu'))
-    model.add(L.BatchNormalization())    
-    model.add(L.MaxPool2D(pool_size=(2,2),padding='same'))
-    model.add(L.Conv2D(filters=16, kernel_size=(3, 3), padding='same',activation='relu'))
+    model = Sequential()
+    model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu',
+                     input_shape=(32, 32, 3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
     
-    model.add(L.Conv2D(filters=8, kernel_size=(3, 3), padding='same',activation='relu'))
-    model.add(L.Conv2D(filters=8, kernel_size=(3, 3), padding='same',activation='relu'))
+    model.add(Conv2D(filters=16, kernel_size=(3, 3),
+                     padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
+    
+    model.add(Conv2D(filters=16, kernel_size=(3, 3),
+                     padding='same', activation='relu'))
 
-    model.add(L.Conv2D(filters=8, kernel_size=(3, 3), padding='same',activation='relu'))
-    model.add(L.Conv2D(filters=8, kernel_size=(3, 3), padding='same',activation='relu'))
+    model.add(Conv2D(filters=8, kernel_size=(
+        3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(filters=8, kernel_size=(
+        3, 3), padding='same', activation='relu'))
 
-    model.add(L.Conv2D(filters=16, kernel_size=(3, 3), padding='same',activation='relu'))
-    model.add(L.BatchNormalization())    
-    model.add(L.Conv2D(filters=16, kernel_size=(3, 3), padding='same',activation='relu'))
-    model.add(L.BatchNormalization())
-    model.add(L.UpSampling2D(size=(2,2),data_format="channels_last"))
-    model.add(L.Conv2D(filters=32, kernel_size=(3, 3), padding='same',activation='relu'))
-    model.add(L.BatchNormalization())
-    model.add(L.UpSampling2D(size=(2,2),data_format="channels_last"))
-    model.add(L.Conv2D(filters=3, kernel_size=(3, 3), padding='same',activation='relu'))
-    model.add(L.BatchNormalization())
-    model.add(L.UpSampling2D(size=(2,2),data_format="channels_last"))
+    model.add(Conv2D(filters=8, kernel_size=(
+        3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(filters=8, kernel_size=(
+        3, 3), padding='same', activation='relu'))
 
+    
+    model.add(Conv2D(filters=16, kernel_size=(
+        3, 3), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(filters=16, kernel_size=(
+        3, 3), padding='same', activation='relu'))
+    
+    model.add(BatchNormalization())
+    model.add(UpSampling2D(size=(2, 2), data_format="channels_last"))
+    
+    model.add(Conv2D(filters=32, kernel_size=(
+        3, 3), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(UpSampling2D(size=(2, 2), data_format="channels_last"))
+    
+    model.add(Conv2D(filters=3, kernel_size=(
+        3, 3), padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(UpSampling2D(size=(2, 2), data_format="channels_last"))
     return model
 
+
 # %%
-'''Load datasets'''
 y_train = []
 X_train = []
 X_test = []
 y_test = []
 
-# path = ('./Linnaeus 5 32x32/train/')
-# path2 = ('./Linnaeus 5 64x64/train/')
-# path_test = ('./Linnaeus 5 32x32/test/')
-# path2_test = ('./Linnaeus 5 64x64/test/')
+path = ('./data/valid_32x32/')
+path2 = ('./data/valid_64x64/')
+path_test = ('./data/Linnaeus 5 32x32/train/')
+path2_test = ('./data/Linnaeus 5 64x64/train/')
 
 
-# for [(r1,d1,f1),(r2,d2,f2),(r3,d3,f3),(r4,d4,f4)] in zip(os.walk(path),os.walk(path2),os.walk(path_test),os.walk(path2_test)):
-#     for files in f1:
-#         X_train.append(cv2.imread(r1 + '/' + files))
-#     for files in f2:
-#         y_train.append(cv2.imread(r2 + '/' + files))
-#     for files in f3:
-#         X_test.append(cv2.imread(r3 + '/' + files))
-#     for files in f4:
-#         y_test.append(cv2.imread(r4 + '/' + files))
+print('Loading training dataset ....')
+for f1 in tqdm(os.listdir(path)):
+    X_train.append(cv2.imread(path + f1))
+    y_train.append(cv2.imread(path2 + f1))
 
-path = ('./data/480/')
-path2 = ('./data/720/')
-for file1 in os.listdir(path):
-    X_train.append(cv2.imread(path+file1))
-    y_train.append(cv2.imread(path2+file1))
+print('''Loading test datasets ....''')
+for [(r1, d1, f1), (r2, d2, f2)] in tqdm(zip(os.walk(path_test), os.walk(path2_test))):
+    for files in f1:
+        X_test.append(cv2.imread(r1 + '/' + files))
+    for files in f2:
+        y_test.append(cv2.imread(r2 + '/' + files))
 
-X_test = X_train[int(len(X_train)*0.9):]
-y_test = y_train[int(len(y_train)*0.9):]
-X_train =  X_train[:int(len(X_train)*0.9)]
-y_train =  y_train[:int(len(y_train)*0.9)]
-
-print(len(X_train),len(y_train),len(X_test),len(y_test))
 # %%
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 X_test = np.array(X_test)
 y_test = np.array(y_test)
-
-print(X_train.shape, y_train.shape, X_test.shape,y_test.shape)
+print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 
 # %%
 model = make_model()
@@ -90,12 +98,13 @@ model.compile(loss=keras.losses.mean_squared_error,
               optimizer=keras.optimizers.adam(lr=0.005),
               metrics=['accuracy'])
 # %%
-from keras.callbacks import TensorBoard
 model.fit(X_train, y_train, batch_size=100, epochs=100, callbacks=[
-          keras.callbacks.ModelCheckpoint('checkpoint2.hdf5'),TensorBoard(log_dir='/SR/new1/autoencoder')],
+          keras.callbacks.ModelCheckpoint('upsampling saved models/checkpoint3.hdf5'), TensorBoard(log_dir='/SR/deep/autoencoder')],
           validation_split=0.2, shuffle=True)
+
+# model.fit_generator()
 # %%
-model.evaluate()
+model.evaluate(x=X_test, y=y_test, batch_size=100, verbose=1)
 # %%
-model.save_weights("weights2.h5")
-model.save("model2.h5")
+model.save_weights("upsampling saved models/weights3.h5")
+model.save("upsampling saved models/model3.h5")
