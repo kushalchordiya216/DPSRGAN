2ecaeb3100140c5f8d254d26cde5e219ed714620
KapilM26 <42468707+KapilM26@users.noreply.github.com>
2019-07-19 00:06:23 +0530

    mod vae

--- code/vaemodel.py
+++ code/vaemodel.py
@@ -8,10 +8,13 @@ tf.reset_default_graph()
 X_in = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 3], name='X')
 Y = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 3], name='Y')
 Y_flat = tf.reshape(Y, shape=[-1, 64 * 64 * 3])
-
+l_test = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 3], name='l_test')
+pred_test = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 3], name='pred_test')
+l_train = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 3], name='l_train')
+pred_train = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 3], name='pred_train')
 keep_prob = tf.placeholder(dtype=tf.float32, shape=(), name='keep_prob')
 n_latent = 512
-num_epochs = 100
+num_epochs = 10
 
 X = []
 Y_im = []
@@ -61,13 +64,17 @@ def decoder(sampled_z, keep_prob):
     with tf.variable_scope("decoder", reuse=None):
         x = tf.layers.dense(sampled_z, units=64 * 64 * 16)
         x = tf.reshape(x, [-1, 64, 64, 16])
-        x = tf.layers.conv2d_transpose(x, filters=16, kernel_size=3, strides=1, padding='same', activation=tf.nn.softmax)
+        x = tf.layers.conv2d_transpose(x, filters=16, kernel_size=3, strides=1, padding='same',
+                                       activation=tf.nn.softmax)
         x = tf.nn.dropout(x, keep_prob)
-        x = tf.layers.conv2d_transpose(x, filters=16, kernel_size=3, strides=1, padding='same', activation=tf.nn.softmax)
+        x = tf.layers.conv2d_transpose(x, filters=16, kernel_size=3, strides=1, padding='same',
+                                       activation=tf.nn.softmax)
         x = tf.nn.dropout(x, keep_prob)
-        x = tf.layers.conv2d_transpose(x, filters=16, kernel_size=3, strides=1, padding='same', activation=tf.nn.softmax)
+        x = tf.layers.conv2d_transpose(x, filters=16, kernel_size=3, strides=1, padding='same',
+                                       activation=tf.nn.softmax)
         x = tf.nn.dropout(x, keep_prob)
-        x = tf.layers.conv2d_transpose(x, filters=16, kernel_size=3, strides=1, padding='same', activation=tf.nn.softmax)
+        x = tf.layers.conv2d_transpose(x, filters=16, kernel_size=3, strides=1, padding='same',
+                                       activation=tf.nn.softmax)
         x = tf.nn.dropout(x, keep_prob)
         img = tf.layers.conv2d_transpose(x, filters=3, kernel_size=3, strides=1, padding='same')
         return img
@@ -84,22 +91,30 @@ optimizer = tf.train.AdamOptimizer(0.005).minimize(loss)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
+
 for i in range(num_epochs):
     print("Epoch %d" % int(i + 1))
     _, loss_val = sess.run([optimizer, loss], feed_dict={X_in: X_train, Y: Y_train, keep_prob: 0.8})
     print("Val loss = %s" % loss_val)
     """
     if not i % 2:
+        init1 = tf.global_variables_initializer()
+        sess.run(init1)
+        init2 = tf.local_variables_initializer()
+        sess.run(init2)
         Y_pred_test = sess.run(img, feed_dict={X_in: X_test, keep_prob: 1})
-        test_acc = tf.metrics.accuracy(Y_test, Y_pred_test)
-        print(test_acc)
-        #print("Test accuracy = %s" % str(test_acc))
+        test_acc, test_op = tf.metrics.accuracy(l_test, pred_test)
+        print(sess)
+        print("Test acc")
+        print(sess.run([test_acc, test_op], feed_dict={l_test: Y_test, pred_test: Y_pred_test}))
         Y_pred_train = sess.run(img, feed_dict={X_in: X_train, keep_prob: 1})
-        train_acc = tf.metrics.accuracy(Y_train, Y_pred_train)
-        print(train_acc)
-        #print("Test accuracy = %s" % str(train_acc))
-    """
+        train_acc, train_op = tf.metrics.accuracy(l_train, pred_train)
+        print("Train acc")
+        print(sess.run([train_acc, train_op], feed_dict={l_train: Y_train, l_test: Y_pred_train}))
+        """
+    
 savePath = saver.save(sess, 'vaemodel1.ckpt')
+
 """
 image = cv2.imread('picc.png')
 image = np.array(image)
